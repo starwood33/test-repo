1a9576fb4c5abee69436f34879433dd36aaf21c4
starwood33 <70668301+starwood33@users.noreply.github.com>
2020-09-07 08:50:47 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,5 @@
-'user': 'admin'
-'password': 'root'
+{
+  "access_token": "9cf6ee24b6a1031e202f292a0ad20c8f52bfd9f01abc8b9489365995052c6603",
+  "token_type": "Bearer",
+  "refresh_token": "a3e5c67af5d8f75034cf23aed24bcfb0d397d6896fe25d5043cce0bd5972639e3ad2d198730ab80959ecf7dcc3c54d07cfd4fc22cb4e1f406e673dc814da84133b7f4ff2bfb800128c"
+}
