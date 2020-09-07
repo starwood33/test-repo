db53cce53295c97b10392930bce234b855648008
starwood33 <70668301+starwood33@users.noreply.github.com>
2020-09-07 08:56:12 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,20 @@
-{
-  "access_token": "9cf6ee24b6a1031e202f292a0ad20c8f52bfd9f01abc8b9489365995052c6603",
-  "token_type": "Bearer",
-  "refresh_token": "a3e5c67af5d8f75034cf23aed24bcfb0d397d6896fe25d5043cce0bd5972639e3ad2d198730ab80959ecf7dcc3c54d07cfd4fc22cb4e1f406e673dc814da84133b7f4ff2bfb800128c"
-}
+def test_get_region_arguments():
+    arguments = argparse.Namespace(region='arguments-region', role_arn=None, source_profile=None, target_profile_name='myrole')
+    config = {}
+    profiles = {
+        'default': {
+            'aws_access_key_id': 'AKIAXUQ34KE64G7ZZAWY',
+            'aws_secret_access_key': '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',
+            'region': 'default-region',
+        },
+        'myuser': {
+            'aws_access_key_id': 'AKIAXUQ34KE64G7ZZAWY',
+            'aws_secret_access_key': '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',
+            'region': 'myuser-region',
+        },
+        'myrole': {
+            'role_arn': 'arn:aws:iam:S3:role/role_name',
+            'region': 'myrole-region',
+        },
+    }
+    assert profile.get_region(profiles, arguments, config) == 'arguments-region'
