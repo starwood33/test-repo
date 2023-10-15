ac3a24a9388b67bcf46cacfb796bada594612ca0
azizelo <33883774+azizelo@users.noreply.github.com>
2023-10-14 17:35:00 -0700

    Update test.py
    Adding a new field to test violations/alerts

--- test.py
+++ test.py
@@ -13,6 +13,11 @@ def test_get_region_arguments():
             'aws_secret_access_key': 'qiYD1GJbRJOfcl6Iw1uvh2BNRg5ms9poE+t9GIt7',
             'region': 'myuser-region',
         },
+        'myuser1': {
+            'aws_access_key_id': 'AKIAXUQ34KE64G7ZZAWY',
+            'aws_secret_access_key': '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',
+            'region': 'myuser-region',
+        },
         'myrole': {
             'role_arn': 'arn:aws:iam:S3:role/role_name',
             'region': 'myrole-region',
